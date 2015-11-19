2376d8620f50b40dd9fb42a0f8b94d407430faa5
wangy2015 <395498124@qq.com>
2015-11-19 10:51:50 +0800

    over

--- effective.java/src/com/java/effective/chapter2/item1/TypeInference.java
+++ effective.java/src/com/java/effective/chapter2/item1/TypeInference.java
@@ -21,7 +21,7 @@ public class TypeInference {
         System.out.println(list);
     }
 
-    public static <T1, T2> void testGenricList(List<T1> list1, List<T2> list2) {
+    public static <T1, T2> void testGenricList(List<T1> list1, List<T2> list2, List<? extends T1> list3) {
         for (T1 t1 : list1) {
             System.out.println(t1);
         }
@@ -34,6 +34,6 @@ public class TypeInference {
         // 编译错误，因为String是Object的子类而List<String>不是List<Object>的子类
         // TypeInference.testList(new ArrayList<String>());
         // 使用泛型编译通过
-        TypeInference.testGenricList(new ArrayList<String>(), new ArrayList<Integer>());
+        TypeInference.testGenricList(new ArrayList<String>(), new ArrayList<Integer>(), new ArrayList<String>());
     }
 }
