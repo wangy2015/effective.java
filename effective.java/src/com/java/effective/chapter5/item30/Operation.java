22a4a79ff24d73d29de5b25284799705dc7ffa47
wangyong <395498124@qq.com>
2016-05-10 21:56:26 +0800

    特定于常量的方法实现

---
+++ effective.java/src/com/java/effective/chapter5/item30/Operation.java
@@ -0,0 +1,18 @@
+package com.java.effective.chapter5.item30;
+
+/**
+ * 特定于常量的方法实现
+ * Created by 王勇 on 2016/5/10.
+ */
+public enum Operation {
+    PLUS { double apply(double x, double y) {return x + y;} },
+    MINUS { double apply(double x, double y) {return x - y;} },
+    TIMES { double apply(double x, double y) {return x * y;} },
+    DIVIDE { double apply(double x, double y) {return x / y;} };
+
+    abstract double apply(double x, double y);
+
+    public static void main(String[] args) {
+        System.out.println(Operation.PLUS.apply(1, 2));
+    }
+}
